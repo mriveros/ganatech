91b25765bcbe12b4d285a6e10feadda09b06c80a
Marcos Riveros <mriveros.py@gmail.com>
2020-04-29 00:01:13 -0400

    odel

---
+++ app/models/v_ganado.rb
@@ -0,0 +1,10 @@
+class VGanado < ActiveRecord::Base
+
+  self.table_name="v_ganados"
+  self.primary_key="ganado_id"
+  
+  attr_accessible :ganado_id, :nombre, :rp, :rp_padre, :rp_madre, :fecha_nacimiento, :codigo_rfid, :potrero_id, :potrero, :peso_promedio, :sexo_ganado_id, :sexo_ganado, :tipo_ganado_id, :tipo_ganado, :raza_id, :raza, :tipo_concepcion_id, :tipo_concepcion, :reproduccion_id, :observacion, :estado_ganado_id, :estado_ganado, :etapa_ganado_id ,:etapa_ganado, :created_at, :updated_at
+  
+  scope :orden_01, -> { order("ganado_id")}
+  
+end
