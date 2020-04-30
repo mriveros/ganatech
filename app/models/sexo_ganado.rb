c46b5d9030208f97713c7acc2add43a4527e9ed0
Marcos Riveros <mriveros.py@gmail.com>
2020-04-30 12:07:07 -0400

    Changes  committed: 	modified:   app/models/sexo_ganado.rb 	modified:   app/views/ganados/_agregar.html.erb 	modified:   app/views/ganados/_editar.html.erb

--- app/models/sexo_ganado.rb
+++ app/models/sexo_ganado.rb
@@ -3,6 +3,6 @@ class SexoGanado < ActiveRecord::Base
   self.table_name="sexos_ganados"
   attr_accessible :id, :descripcion
   
-  scope :orden_id, -> {order("id")}
+  scope :orden_01, -> {order("id")}
 
 end
