974aa73a2d13088ef80c765ca5328369a41231db
Marcos Riveros <mriveros.py@gmail.com>
2020-05-10 20:11:57 -0400

    Changes to be committed: 	modified:   app/models/v_celo.rb 	modified:   app/views/celos/_lista.html.erb

--- app/models/v_celo.rb
+++ app/models/v_celo.rb
@@ -3,8 +3,8 @@ class VCelo < ActiveRecord::Base
   self.table_name="v_celos"
   self.primary_key="celo_id"
   
-  attr_accessible :celo_id, :ganado_rfid, :ganado_nombre, :ganado_rp,
-  :fecha_inicio, :fecha_fin, :descripcion, :hacienda_id, :hacienda, 
+  attr_accessible :celo_id, :ganado_id, :codigo_rfid, :ganado_nombre, :ganado_rp,
+  :fecha_inicio, :fecha_fin, :descripcion, :potrero_id, :potrero,:hacienda_id, :hacienda, 
   :estado_celo_id, :estado_celo, :observacion, :created_at, :updated_at
   scope :orden_01, -> { order("celo_id")}
   
