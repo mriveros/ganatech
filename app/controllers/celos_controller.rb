d0171e14bb7312f8eb14c1334704baadff24234d
Marcos Riveros <mriveros.py@gmail.com>
2020-05-12 22:26:08 -0400

    Changes to be committed: 	modified:   app/controllers/celos_controller.rb 	modified:   app/models/tipo_concepcion.rb 	modified:   app/views/celos/_cambiar_estado_a_reproduccion.html.erb 	modified:   config/parametros.yml
     Changes not staged for commit:
    	modified:   config/routes.rb

--- app/controllers/celos_controller.rb
+++ app/controllers/celos_controller.rb
@@ -377,5 +377,18 @@ before_filter :require_usuario
 
   end
 
+  def buscar_ganado_reproductor
+
+    @ganados = VGanado.where("nombre ilike ? and sexo_ganado_id = ? and etapa_ganado_id in (?)", "%#{params[:ganado]}%", params[:sexo_ganado_id], [PARAMETRO[:etapa_ganado_torito], PARAMETRO[:etapa_ganado_toro]])
+
+    respond_to do |f|
+      
+      f.html
+      f.json { render :json => @ganados }
+    
+    end
+
+  end
+
 
 end
--- app/models/tipo_concepcion.rb
+++ app/models/tipo_concepcion.rb
@@ -6,6 +6,7 @@ class TipoConcepcion < ActiveRecord::Base
   attr_accessible :id, :descripcion, :created_at, :updated_at
  
   scope :orden_01, -> { order("id")}
+  scope :tipo_concepcion, -> { where("id in (1,2)")}
 
   
 end
