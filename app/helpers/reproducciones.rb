745d44bb2081d7ad3b019bd7af0b9d551e322497
Marcos Riveros <mriveros.py@gmail.com>
2020-05-14 21:57:19 -0400

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	new file:   app/helpers/reproducciones.rb

---
+++ app/helpers/reproducciones.rb
@@ -0,0 +1,34 @@
+module ReproduccionesHelper
+
+  def link_to_editar_celo(celo_id)
+
+      render partial: 'link_to_editar_celo', locals: { celo_id: celo_id }
+      
+  end
+
+  def link_to_celo_detalle(celo_id)
+
+    render partial: 'link_to_celo_detalle', locals: { celo_id: celo_id}
+
+  end
+
+  def link_to_cambiar_estado_celo_a_fecundacion(celo_id)
+
+    render partial: 'link_to_cambiar_estado_celo_a_fecundacion', locals: {celo_id: celo_id}
+
+  end
+
+  def link_to_cambiar_estado_a_en_reproduccion(celo_id)
+
+    render partial: 'link_to_cambiar_estado_a_en_reproduccion', locals: {celo_id: celo_id}
+
+  end
+
+
+  def link_to_cambiar_estado_a_celo_perdido(celo_id)
+
+    render partial: 'link_to_cambiar_estado_a_celo_perdido', locals: {celo_id: celo_id}
+
+  end
+  
+end
