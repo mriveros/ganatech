8531b2aadeece8bfcf640f4c0d82d3022354a7e7
Marcos Riveros <mriveros.py@gmail.com>
2020-05-25 23:10:48 -0400

    better

--- app/controllers/reproducciones_controller.rb
+++ app/controllers/reproducciones_controller.rb
@@ -389,7 +389,6 @@ before_filter :require_usuario
     @valido = true
 
     @celo = Celo.where("id = ?", params[:celo_id]).first
-    auditoria_id_celo = auditoria_antes("actualizar estado del celo en guardar celo en reproduccion", "celos", @celo)
     @ganado = Ganado.where('id = ?', @celo.ganado_id).first
     auditoria_id_ganado = auditoria_antes("actualizar estado del ganado en guardar celo en reproduccion", "ganados", @ganado)
     
@@ -397,45 +396,7 @@ before_filter :require_usuario
 
     Ganado.transaction do
 
-      @reproduccion = Reproduccion.new
-      @reproduccion.celo_id = params[:celo_id]
-      @reproduccion.tipo_concepcion_id = params[:tipo_concepcion][:id]
-      if params[:tipo_concepcion][:id].to_i == PARAMETRO[:tipo_concepcion_monta_natural].to_i
-
-        @reproduccion.ganado_reproductor_id = params[:ganado_reproductor_id]
-
-      else
-
-        @reproduccion.esperma_id = params[:esperma_id]
-
-      end
-
-      @reproduccion.fecha_reproduccion = params[:fecha_reproduccion]
-      @reproduccion.fecha_concepcion = params[:fecha_concepcion]
-      @reproduccion.estado_reproduccion_id = PARAMETRO[:estado_reproduccion_proceso_fecundacion]
-      @reproduccion.descripcion = params[:descripcion]
-      @reproduccion.observacion = params[:observacion]
-
-      if @reproduccion.save
-
-        auditoria_nueva("agregar reproduccion en modulo de celos", "reproducciones", @reproduccion)
-        @celo.estado_celo_id = PARAMETRO[:estado_celo_en_reproduccion]
-        
-        if @celo.save
-         
-          auditoria_despues(@celo, auditoria_id_celo)
-          @ganado.estado_ganado_id = PARAMETRO[:estado_ganado_en_reproduccion]
-          
-          if @ganado.save
-
-            auditoria_despues(@ganado, auditoria_id_ganado)
-            @guardado_ok = true
-
-          end
-
-        end
-        
-      end
+      
 
     end # end transaction
 
