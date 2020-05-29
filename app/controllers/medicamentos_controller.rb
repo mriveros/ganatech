11a1545ba6fb3dc896740d6f6ea1dd76b22a50c6
Marcos Riveros <mriveros.py@gmail.com>
2020-05-29 19:11:56 -0400

    Changes  committed: 	modified:   app/controllers/medicamentos_controller.rb 	new file:   app/models/v_medicamento.rb

--- app/controllers/medicamentos_controller.rb
+++ app/controllers/medicamentos_controller.rb
@@ -3,9 +3,117 @@ class MedicamentosController < ApplicationController
   before_filter :require_usuario
 
   def index
-
-
     
+    cond = []
+    args = []
+
+    if params[:form_buscar_medicamento_id].present?
+
+      cond << "medicamento_id = ?"
+      args << params[:form_buscar_medicamento_id]
+
+    end
+
+    if params[:form_medicamento_nombre_medicamento].present?
+
+      cond << "nombre_medicamento  ilike ?"
+      args << "%#{params[:form_medicamento_nombre_medicamento]}%"
+
+    end
+
+    if params[:form_medicamento_descripcion].present?
+
+      cond << "medicamento  ilike ?"
+      args << "%#{params[:form_medicamento_descripcion]}%"
+
+    end
+
+    if params[:form_buscar_medicamento_cantidad_stock].present?
+
+      cond << "cantidad_stock  = ?"
+      args << params[:form_buscar_medicamento_cantidad_stock]
+
+    end
+
+    if params[:form_buscar_medicamento_cantidad_aplicacion].present?
+
+      cond << "cantidad_aplicacion = ?"
+      args << params[:form_buscar_medicamento_cantidad_aplicacion]
+
+    end
+
+    if params[:form_buscar_medicamento_ciclo].present?
+
+      cond << "ciclo = ?"
+      args << params[:form_buscar_medicamento_ciclo]
+
+    end
+
+
+    if params[:form_buscar_medicamento_intervalo_tiempo].present?
+
+      cond << "intervalo_tiempo = ?"
+      args << params[:form_buscar_medicamento_intervalo_tiempo]
+
+    end
+
+    if params[:form_buscar_medicamento][:estado_medicamento_id].present?
+
+      cond << "estado_medicamento_id = ?"
+      args << params[:form_buscar_medicamento][:estado_medicamento_id]
+
+    end
+
+    if params[:form_buscar_medicamento][:tipo_presentacion_id].present?
+
+      cond << "tipo_presentacion_id = ?"
+      args <<params[:form_buscar_medicamento][:tipo_presentacion_id]
+
+    end
+
+    if params[:form_buscar_medicamento][:tipo_administracion_id].present?
+
+      cond << "tipo_administracion_id = ?"
+      args <<params[:form_buscar_medicamento][:tipo_administracion_id]
+
+    end
+
+    if params[:form_buscar_potrero][:departamento_id].present?
+
+      cond << "departamento_id = ?"
+      args << params[:form_buscar_potrero][:departamento_id]
+
+    end
+
+    if params[:form_buscar_medicamento_fecha_vencimiento].present?
+
+      cond << "fecha_vencimiento = ?"
+      args << params[:form_buscar_medicamento_fecha_vencimiento]
+
+    end
+
+
+    cond = cond.join(" and ").lines.to_a + args if cond.size > 0
+
+    if cond.size > 0
+
+      @potreros =  VPotrero.orden_01.where(cond).paginate(per_page: 10, page: params[:page])
+      @total_encontrados = VPotrero.where(cond).count
+
+    else
+     
+      @potreros = VPotrero.orden_01.paginate(per_page: 10, page: params[:page])
+      @total_encontrados = VPotrero.count
+
+    end
+
+    @total_registros = VPotrero.count
+
+    respond_to do |f|
+      
+     f.js
+      
+    end
 
   end
 
---
+++ app/models/v_medicamento.rb
@@ -0,0 +1,13 @@
+class VMedicamento < ActiveRecord::Base
+
+  self.table_name="v_medicamentos"
+
+  attr_accessible :medicamento_id, :medicamento, :nombre_medicamento, :cantidad_stock, :cantidad_aplicacion,
+  :ciclo, :intervalo_tiempo, :observacion, :estado_medicamento_id, :estado_medicamento,
+  :tipo_presentacion_id, :tipo_presentacion, :tipo_administracion_id, :tipo_administracion,
+  :fecha_vencimiento, :created-at, :updated_at
+  
+  scope :orden_01, -> {order("medicamento_id")}
+
+  
+end
