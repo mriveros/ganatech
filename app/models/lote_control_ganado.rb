6480c539dba2c4ee59e84d50a00bdcec314b983b
Marcos Riveros <mriveros.py@gmail.com>
2020-06-26 00:01:52 -0400

    adding tmp tales model

---
+++ app/models/lote_control_ganado.rb
@@ -0,0 +1,11 @@
+class LoteControlGanado < ActiveRecord::Base
+
+  self.table_name="tmp_ganados_controles_lotes"
+  attr_accessible :id, :ganado_id, :created_at, :updated_at
+  
+  scope :orden_01, -> {order("id")}
+
+  scope :orden_ganado, -> { order("ganado_id")}
+
+  
+end
