aa7ca6f449be591d8c6bfcf6cbe9f4b30379f44e
Juanma Yegros <juanmayegros@gmail.com>
2020-06-30 22:48:53 -0400

    fix alimentacion

--- app/helpers/alimentaciones_helper.rb
+++ app/helpers/alimentaciones_helper.rb
@@ -1,16 +1,16 @@
 module AlimentacionesHelper
 
-def link_to_alimentacion_detalle(alimentacion_id)
+  def link_to_alimentacion_detalle(alimentacion_id)
 
     render partial: 'link_to_alimentacion_detalle', locals: { alimentacion_id: alimentacion_id }
 
-end
+  end
 
 
-def link_to_editar_alimentacion(alimentacion_id)
+  def link_to_editar_alimentacion(alimentacion_id)
 
-  render partial: 'link_to_editar_alimentacion', locals: {alimentacion_id: alimentacion_id}
+    render partial: 'link_to_editar_alimentacion', locals: {alimentacion_id: alimentacion_id}
 
-end
+  end
 
 end
