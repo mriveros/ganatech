05b41f1676354583732440fe04d2ff13a8c263ff
Marcos Riveros <mriveros.py@gmail.com>
2020-08-24 21:41:15 -0400

    modified:   app/models/ganado_enfermo.rb

--- app/models/ganado_enfermo.rb
+++ app/models/ganado_enfermo.rb
@@ -3,7 +3,7 @@ class GanadoEnfermo < ActiveRecord::Base
   self.table_name="ganados_enfermos"
   self.primary_key="id"
   
-  attr_accessible :id, :ganado_id, :enfermedad_id, :fecha, :observacion, :created_at,  :updated_at;, :documento_ganatec_id
+  attr_accessible :id, :ganado_id, :enfermedad_id, :fecha, :observacion, :created_at,  :updated_at, :documento_ganatec_id
   
   scope :orden_01, -> { order("id")}
   
