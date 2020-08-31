7a0cd2d54f8d35a7ef4f3680b01cee8522dc7fda
Marcos Riveros <mriveros.py@gmail.com>
2020-08-30 22:47:49 -0400

    modified:   app/helpers/altas_producciones_helper.rb 	modified:   app/views/altas_producciones/_alta_produccion_detalle.html.erb 	new file:   app/views/altas_producciones/agregar_produccion_lote.js.erb 	new file:   app/views/altas_producciones/eliminar_produccion_lote.js.erb

--- app/helpers/altas_producciones_helper.rb
+++ app/helpers/altas_producciones_helper.rb
@@ -4,14 +4,19 @@ module AltasProduccionesHelper
 
       render partial: 'link_to_alta_produccion_detalle', locals: { alta_produccion_id: alta_produccion_id }
       
-  end
+  	end
 
-  def link_to_editar_alta_produccion(alta_produccion_id)
+  	def link_to_editar_alta_produccion(alta_produccion_id)
 
       render partial: 'link_to_editar_alta_produccion', locals: { alta_produccion_id: alta_produccion_id }
       
-  end
+  	end
 
-  
+  	def verificar_produccion_lote(alta_produccion_detalle_id)
+
+      lote_produccion_ganado = LoteProduccionGanado.where("alta_produccion_detalle_id = ?", alta_produccion_detalle_id).first
+      return (lote_produccion_ganado.present?)? true : false
+      
+  	end
   
 end
