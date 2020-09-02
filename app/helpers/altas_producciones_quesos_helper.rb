22b582dfe0365880c04ae20f60785f06f29e5d30
Marcos Riveros <mriveros.py@gmail.com>
2020-09-02 11:10:09 -0400

    modified:   app/helpers/altas_producciones_quesos_helper.rb 	deleted:    app/views/altas_producciones_quesos/_agregar_alta_produccion_detalle.html.erb 	deleted:    app/views/altas_producciones_quesos/_agregar_alta_produccion_queso.html.erb 	modified:   app/views/altas_producciones_quesos/_lista.html.erb eleted:    app/views/altas_producciones_quesos/actualizar.js.erb deleted:    app/views/altas_producciones_quesos/agregar.js.erb 	deleted:    app/views/altas_producciones_quesos/agregar_alta_produccion_detalle.js.erb 	deleted:    app/views/altas_producciones_quesos/agregar_alta_produccion_queso.js.erb 	deleted:    app/views/altas_producciones_quesos/agregar_produccion_lote.js.erb 	deleted:    app/views/altas_producciones_quesos/eliminar_alta_produccion_detalle.js.erb 	deleted:    app/views/altas_producciones_quesos/eliminar_produccion_lote.js.erb 	renamed:    app/views/altas_producciones_quesos/marcar_desactivado.js.erb -> app/views/altas_producciones_quesos/enviar_a_derivados_lacteos.js.erb 	deleted:    app/views/altas_producciones_quesos/guardar_alta_produccion_detalle.js.erb 	deleted:    app/views/altas_producciones_quesos/guardar_alta_produccion_queso.js.erb 	modified:   app/views/altas_producciones_quesos/index.html.erb 	modified:   app/views/altas_producciones_quesos/lista.js.erb 	modified:   app/views/layouts/_menu.html.erb

--- app/helpers/altas_producciones_quesos_helper.rb
+++ app/helpers/altas_producciones_quesos_helper.rb
@@ -1,22 +1,9 @@
 module AltasProduccionesQuesosHelper
 
-	def link_to_alta_produccion_detalle(alta_produccion_id)
+	def link_to_alta_produccion_queso_detalle(alta_produccion_queso_id)
 
-      render partial: 'link_to_alta_produccion_detalle', locals: { alta_produccion_id: alta_produccion_id }
+      render partial: 'link_to_alta_produccion_queso_detalle', locals: { alta_produccion_queso_id: alta_produccion_queso_id }
       
   	end
 
-  	def link_to_editar_alta_produccion(alta_produccion_id)
-
-      render partial: 'link_to_editar_alta_produccion', locals: { alta_produccion_id: alta_produccion_id }
-      
-  	end
-
-  	def verificar_produccion_lote(alta_produccion_detalle_id)
-
-      lote_produccion_ganado = LoteProduccionGanado.where("alta_produccion_detalle_id = ?", alta_produccion_detalle_id).first
-      return (lote_produccion_ganado.present?)? true : false
-      
-  	end
-  
 end
