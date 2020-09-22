65593d31a4923047b436c8b3e45a45e4d3c77e5d
Marcos Riveros <mriveros.py@gmail.com>
2020-09-21 22:02:18 -0400

    modified:   app/models/personal.rb

--- app/models/personal.rb
+++ app/models/personal.rb
@@ -1,10 +1,11 @@
 class Personal < ActiveRecord::Base
 
-  self.table_name="personales"
+  self.table_name="personales" 
   self.primary_key="id"
   
   attr_accessible :id, :nombre, :apellido, :ruc_ci, :direccion, :telefono, :email, :cargo_id, :hacienda_id, :estado_personal_id, :observacion
   
+  scope :nombre_apellido, -> { select("nombre || ' ' || apellido as nombre_apellido")} 
   scope :orden_01, -> { order("id")}
   
 end
