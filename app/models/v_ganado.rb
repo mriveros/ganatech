0c1439a8e825ecfa0cbbf0825940df327123a03e
Marcos Riveros <mriveros.py@gmail.com>
2020-09-25 13:44:02 -0400

    modified:   app/models/v_ganado.rb

--- app/models/v_ganado.rb
+++ app/models/v_ganado.rb
@@ -7,7 +7,7 @@ class VGanado < ActiveRecord::Base
   
   scope :orden_01, -> { order("ganado_id")}
 
-  scope :modulo_ganado_salida, -> { where("estado_ganado_id not in (?)", [PARAMETRO[:estado_ganado_en_proceso_venta], PARAMETRO[:estado_ganado_vendido]])}
+  scope :modulo_ganado_salida, -> { where("estado_ganado_id not in (?)", [PARAMETRO[:estado_ganado_en_proceso_venta], PARAMETRO[:estado_ganado_vendido], PARAMETRO[:estado_ganado_muerto]])}
   
   scope :group_01, -> {group("estado_ganado, ganado_id, nombre,rp, rp_padre ,rp_madre,fecha_nacimiento,codigo_rfid,hacienda_id,hacienda, potrero_id,potrero,peso_promedio,  
 	sexo_ganado_id, sexo_ganado,tipo_ganado_id, tipo_ganado, raza_id ,raza, tipo_concepcion_id ,tipo_concepcion,reproduccion_id,
@@ -18,7 +18,7 @@ class VGanado < ActiveRecord::Base
 
   scope :modulo_control_ganado, -> { where("estado_ganado_id not in (?)", [PARAMETRO[:estado_ganado_vendido], PARAMETRO[:estado_ganado_muerto]])}
 
-   scope :modulo_control_alimento, -> { where("estado_ganado_id not in (?)", [PARAMETRO[:estado_ganado_vendido], PARAMETRO[:estado_ganado_muerto]])}
+  scope :modulo_control_alimento, -> { where("estado_ganado_id not in (?)", [PARAMETRO[:estado_ganado_vendido], PARAMETRO[:estado_ganado_muerto]])}
 
   scope :modulo_ganados, -> { where("estado_ganado_id not in (?)", [PARAMETRO[:estado_ganado_vendido]])}
 
