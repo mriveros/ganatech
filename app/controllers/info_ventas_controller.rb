b509fb50aee48b6d20a2e358f6d3bc2bdb9d4074
Marcos Riveros <mriveros.py@gmail.com>
2020-09-30 19:07:42 -0400

    modified:   app/controllers/info_ventas_controller.rb 	new file:   app/views/info_gastos/_detalles_planilla_resumen_gasto.pdf.erb 	new file:   app/views/info_gastos/cabecera_planilla_resumen_gasto.pdf.erb 	new file:   app/views/info_gastos/planilla_reporte_gasto.pdf.erb 	new file:   app/views/info_ventas/_detalles_planilla_resumen_venta.pdf.erb 	new file:   app/views/info_ventas/cabecera_planilla_resumen_venta.pdf.erb 	modified:   app/views/info_ventas/lista.js.erb 	new file:   app/views/info_ventas/planilla_reporte_venta.pdf.erb

--- app/controllers/info_ventas_controller.rb
+++ app/controllers/info_ventas_controller.rb
@@ -76,12 +76,46 @@ before_filter :require_usuario
     end
 
     @total_registros = AuxVenta.count
+    
+    @parametros = { format: :pdf, venta_id: @aux_ventas.map(&:id), fecha: params[:form_buscar_info_compras_fecha], descripcion: params[:form_buscar_info_compras_descripcion], monto: params[:form_buscar_info_compras_monto], observacion: params[:form_buscar_info_compras_fecha_desde], fecha_desde: params[:form_buscar_info_compras_fecha_desde], fecha_hasta: params[:form_buscar_info_compras_fecha_hasta]}
 
     respond_to do |f|
       
      f.js
       
     end
+
+    def exportar_pdf
+    
+          
+    @aux_ventas =  AuxVenta.where("id in (?)", params[:venta_id]).orden_01.paginate(per_page: 10, page: params[:page])
+      
+    respond_to do |f|
+      
+      f.pdf do
+
+          render  :pdf => "planilla_resumen_venta_#{Time.now.strftime("%Y_%m_%d__%H_%M")}",
+                  :template => 'info_ventas/planilla_reporte_venta.pdf.erb',
+                  :layout => 'pdf.html',
+                  :header => {:html => { :template => "info_ventas/cabecera_planilla_resumen_venta.pdf.erb" ,
+                  :locals   => { :venta => @aux_ventas }}},
+                  :margin => {:top => 65,
+                  :bottom => 11,
+                  :left => 3,
+                  :right => 3},
+                  :orientation => 'Landscape',
+                  :page_size => "A4",
+                  :footer => { :html => {:template => 'layouts/footer.pdf' },
+                  :spacing => 1,
+                  :line => true }
+
+      end
+      
+    end
+  
+  end
+
+
 
   end
 
