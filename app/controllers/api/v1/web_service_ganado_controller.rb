a1acda8c70900f6df77a0eab8a0a17ec99c4a0da
Marcos Riveros <mriveros@Marcoss-MacBook-Air.local>
2020-11-19 08:44:30 -0300

    modified:   app/controllers/api/v1/web_service_ganado_controller.rb

--- app/controllers/api/v1/web_service_ganado_controller.rb
+++ app/controllers/api/v1/web_service_ganado_controller.rb
@@ -7,14 +7,28 @@ module Api
 			protect_from_forgery with: :null_session
 			respond_to :json, only: [:create]
 
+			require 'json'
+
+			
+
   			def show
 
 				@response = VGanado.modulo_ganados
+				
+				json = JSON.parse(@response.to_json)
+				json.first.reject! do |key, value|
+
+				  value == ''
+
+				end
+				
+				puts json.to_s
 
 				respond_to do |f|
 
-			      f.json { render :json => @response.to_json}
+			      f.json { render :json => json.to_s}
 
+			   
 			    end
 
 
