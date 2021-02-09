d267031be69322262824fc35eb41f30836f9dfcb
Marcos Riveros <mriveros.py@gmail.com>
2021-02-09 17:31:04 -0300

    modified:   app/controllers/alimentaciones_controller.rb

--- app/controllers/alimentaciones_controller.rb
+++ app/controllers/alimentaciones_controller.rb
@@ -152,6 +152,7 @@ class AlimentacionesController < ApplicationController
       @alimento.estado_alimento_id = params[:estado_alimento][:id]
       @alimento.tipo_alimentacion_id = params[:tipo_alimentacion][:id]
       @alimento.fecha_vencimiento = params[:fecha_vencimiento]
+      @alimento.cantidad_porciones = params[:cantidad_stock] * params[:cantidad_porciones]
 
       if @alimento.save
 
@@ -291,6 +292,7 @@ class AlimentacionesController < ApplicationController
       @alimento.estado_alimento_id = params[:alimentacion][:estado_alimento_id]
       @alimento.tipo_alimentacion_id = params[:alimentacion][:tipo_alimentacion_id]
       @alimento.fecha_vencimiento = params[:alimentacion][:fecha_vencimiento]
+      @alimento.cantidad_porciones = params[:cantidad_stock] * params[:cantidad_porciones]
       
 
       if @alimento.save
@@ -395,6 +397,8 @@ class AlimentacionesController < ApplicationController
           auditoria_nueva("agregar alimentacion detalle", "alimentaciones_detalles", @alimentacion_detalle)
 
           @alimentacion.cantidad_stock = @alimentacion.cantidad_stock + @alimentacion_detalle.cantidad_suministro
+          @alimentacion.cantidad_porciones = @alimentacion.cantidad_porciones + (params[:cantidad_suministro] * params[:cantidad_porciones])
+          
           if @alimentacion.save
 
             auditoria_despues(@alimentacion, auditoria_id)
