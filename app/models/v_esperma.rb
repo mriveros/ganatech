b715f25c4cea050161a9079db51c9fb5fae08143
Marcos Riveros <mriveros.py@gmail.com>
2021-02-23 15:55:45 -0300

    modified:   app/models/v_esperma.rb 	modified:   app/views/espermas/_esperma_detalle.html.erb

--- app/models/v_esperma.rb
+++ app/models/v_esperma.rb
@@ -6,7 +6,8 @@ class VEsperma < ActiveRecord::Base
   attr_accessible  :esperma_id, :descripcion, :ganado_id, :nombre_ganado, :ganado_rp,
   :numero_pajuela, :raza_id, :raza_ganado, :observacion, :estado_esperma_id, 
   :estado_esperma, :esperma_procedencia_id, :esperma_procedencia, :costo_esperma,
-  :fecha_registro, :costo_total, :cantidad, :created_at, :updated_at, :cantidad_inicial
+  :fecha_registro, :costo_total, :cantidad, :created_at, :updated_at, :cantidad_inicial,
+  :documento_ganatec_id
   
   scope :orden_01, -> { order("esperma_id")}
   scope :orden_fecha, -> { order("fecha_registro desc")}
