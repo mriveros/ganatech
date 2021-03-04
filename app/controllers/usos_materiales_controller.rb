ce10e9a59e10e4eb31be9f6fc9a3cafebed8f336
Marcos Riveros <mriveros.py@gmail.com>
2021-03-04 19:38:36 -0300

    modified:   app/controllers/usos_materiales_controller.rb

--- app/controllers/usos_materiales_controller.rb
+++ app/controllers/usos_materiales_controller.rb
@@ -12,80 +12,80 @@ before_filter :require_usuario
     cond = []
     args = []
 
-    if params[:form_buscar_control_alimentacion_id].present?
+    if params[:form_buscar_uso_material_id].present?
 
-      cond << "control_alimentacion_id = ?"
-      args << params[:form_buscar_control_alimentacion_id]
+      cond << "uso_material_id = ?"
+      args << params[:form_buscar_uso_material_id]
 
     end
 
-    if params[:form_buscar_control_alimentacion_codigo].present?
+    if params[:form_buscar_uso_material_codigo].present?
 
       cond << "codigo_lote = ?"
-      args << params[:form_buscar_control_alimentacion_codigo]
+      args << params[:form_buscar_uso_material_codigo]
 
     end
 
-    if params[:form_buscar_control_alimentacion_nombre].present?
+    if params[:form_buscar_uso_material_nombre].present?
 
       cond << "ganado_nombre ilike ?"
-      args << "%#{params[:form_buscar_control_alimentacion_nombre]}%"
+      args << "%#{params[:form_buscar_uso_material_nombre]}%"
 
     end
 
-    if params[:form_buscar_control_alimentacion_ganado_rp].present?
+    if params[:form_buscar_uso_material_ganado_rp].present?
 
       cond << "ganado_rp  ilike ?"
-      args << "%#{params[:form_buscar_control_alimentacion_ganado_rp]}%"
+      args << "%#{params[:form_buscar_uso_material_ganado_rp]}%"
 
     end
 
-    if params[:form_buscar_control_alimentacion][:tipo_alimentacion_id].present?
+    if params[:form_buscar_uso_material][:tipo_alimentacion_id].present?
 
       cond << "tipo_alimentacion_id = ?"
-      args << params[:form_buscar_control_alimentacion][:tipo_alimentacion_id]
+      args << params[:form_buscar_uso_material][:tipo_alimentacion_id]
 
     end
 
-    if params[:form_buscar_control_alimentacion][:alimentacion_id].present?
+    if params[:form_buscar_uso_material][:alimentacion_id].present?
 
       cond << "alimentacion_id = ?"
-      args << params[:form_buscar_control_alimentacion][:alimentacion_id]
+      args << params[:form_buscar_uso_material][:alimentacion_id]
 
     end
 
-    if params[:form_buscar_control_alimentacion_cantidad_suministrada].present?
+    if params[:form_buscar_uso_material_cantidad_suministrada].present?
 
       cond << "cantidad_suministrada  = ?"
-      args << params[:form_buscar_control_alimentacion_cantidad_suministrada]
+      args << params[:form_buscar_uso_material_cantidad_suministrada]
 
     end
 
-    if params[:form_buscar_control_alimentacion_peso].present?
+    if params[:form_buscar_uso_material_peso].present?
 
       cond << "peso = ?"
-      args << params[:form_buscar_control_alimentacion_peso]
+      args << params[:form_buscar_uso_material_peso]
 
     end
 
-    if params[:form_buscar_control_alimentacion_fecha_control].present?
+    if params[:form_buscar_uso_material_fecha_control].present?
 
       cond << "fecha_control = ?"
-      args << params[:form_buscar_control_alimentacion_fecha_control]
+      args << params[:form_buscar_uso_material_fecha_control]
 
     end
 
-    if params[:form_buscar_control_alimentacion_observacion].present?
+    if params[:form_buscar_uso_material_observacion].present?
 
       cond << "observacion ilike ?"
-      args << "%#{params[:form_buscar_control_alimentacion_observacion]}%"
+      args << "%#{params[:form_buscar_uso_material_observacion]}%"
 
     end
 
-    if params[:form_buscar_control_alimentacion][:clasificacion_alimentacion_id].present?
+    if params[:form_buscar_uso_material][:clasificacion_alimentacion_id].present?
 
       cond << "clasificacion_alimentacion_id = ?"
-      args << params[:form_buscar_control_alimentacion][:clasificacion_alimentacion_id]
+      args << params[:form_buscar_uso_material][:clasificacion_alimentacion_id]
 
     end
 
@@ -94,17 +94,17 @@ before_filter :require_usuario
 
     if cond.size > 0
 
-      @controles_alimentaciones =  VControlAlimentacion.orden_01.where(cond).paginate(per_page: 10, page: params[:page])
-      @total_encontrados = VControlAlimentacion.where(cond).count
+      @controles_alimentaciones =  VUsoMaterial.orden_01.where(cond).paginate(per_page: 10, page: params[:page])
+      @total_encontrados = VUsoMaterial.where(cond).count
 
     else
      
-      @controles_alimentaciones = VControlAlimentacion.orden_01.paginate(per_page: 10, page: params[:page])
-      @total_encontrados = VControlAlimentacion.count
+      @controles_alimentaciones = VUsoMaterial.orden_01.paginate(per_page: 10, page: params[:page])
+      @total_encontrados = VUsoMaterial.count
 
     end
 
-    @total_registros = VControlAlimentacion.count
+    @total_registros = VUsoMaterial.count
 
     respond_to do |f|
       
@@ -116,8 +116,8 @@ before_filter :require_usuario
 
   def agregar
 
-    @control_alimentacion = ControlAlimentacion.new
-    nuevo_incremento = ControlAlimentacion.last
+    @uso_material = UsoMaterial.new
+    nuevo_incremento = UsoMaterial.last
     if nuevo_incremento.present?
 
       @codigo_control = nuevo_incremento.codigo_lote + 1
@@ -145,7 +145,7 @@ before_filter :require_usuario
 
       if params[:clasificacion_alimentacion][:id].to_i == PARAMETRO[:clasificacion_por_ganado]
         
-        @alimentacion = ControlAlimentacion.new
+        @alimentacion = UsoMaterial.new
         @alimentacion.ganado_id = params[:ganado_id]
         @alimentacion.fecha_control = params[:fecha_control]
         @alimentacion.tipo_alimentacion_id = params[:tipo_alimentacion][:id]
@@ -177,7 +177,7 @@ before_filter :require_usuario
     @eliminado = false
     @msg = ""
 
-    @alimentacion = ControlAlimentacion.where("id = ?", params[:control_alimentacion_id]).first
+    @alimentacion = UsoMaterial.where("id = ?", params[:uso_material_id]).first
     @alimentacion_elim = @alimentacion
     
     if @alimentacion.destroy
