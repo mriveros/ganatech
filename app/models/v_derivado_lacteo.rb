1cdb435091fdceed08a3f69be7c8d824b3f8a31b
Marcos Riveros <mriveros.py@gmail.com>
2021-04-05 21:15:20 -0400

    modified:   app/controllers/derivados_lacteos_controller.rb 	modified:   app/models/v_derivado_lacteo.rb

--- app/models/v_derivado_lacteo.rb
+++ app/models/v_derivado_lacteo.rb
@@ -6,5 +6,7 @@ class VDerivadoLacteo < ActiveRecord::Base
   attr_accessible :derivado_lacteo_id, :tipo_derivado_id, :tipo_derivado, :cantidad_inicial, :cantidad_actual, :tipo_medicion_id, :tipo_medicion,
   :alta_produccion_queso_id, :periodo_produccion_queso, :alta_produccion_detalle_id, :estado_derivado_lacteo_id, :estado_derivado_lacteo
   scope :orden_01, -> { order("derivado_lacteo_id")}
+  scope :orden_cantidad, -> { order("cantidad_actual desc")}
+
   
 end
