d309806b2925843f9fe6e9e374ecb59018a25294
Marcos Riveros <mriveros.py@gmail.com>
2021-04-12 20:34:55 -0400

    renamed:    app/controllers/notificaciones_usuarios_controller.rb -> app/controllers/notificaciones_personas_controller.rb 	modified:   app/models/notificacion_usuario.rb 	renamed:    app/views/notificaciones_usuarios/_agregar.html.erb -> app/views/notificaciones_personas/_agregar.html.erb 	renamed:    app/views/notificaciones_usuarios/_editar.html.erb -> app/views/notificaciones_personas/_editar.html.erb 	renamed:    app/views/notificaciones_usuarios/_link_to_editar_notificacion_usuario.html.erb -> app/views/notificaciones_personas/_link_to_editar_notificacion_usuario.html.erb 	renamed:    app/views/notificaciones_usuarios/_lista.html.erb -> app/views/notificaciones_personas/_lista.html.erb 	renamed:    app/views/notificaciones_usuarios/actualizar.js.erb -> app/views/notificaciones_personas/actualizar.js.erb 	renamed:    app/views/notificaciones_usuarios/agregar.js.erb -> app/views/notificaciones_personas/agregar.js.erb 	renamed:    app/views/notificaciones_usuarios/editar.js.erb -> app/views/notificaciones_personas/editar.js.erb 	renamed:    app/views/notificaciones_usuarios/eliminar.js.erb -> app/views/notificaciones_personas/eliminar.js.erb 	renamed:    app/views/notificaciones_usuarios/guardar.js.erb -> app/views/notificaciones_personas/guardar.js.erb 	renamed:    app/views/notificaciones_usuarios/index.html.erb -> app/views/notificaciones_personas/index.html.erb#	renamed:    app/views/notificaciones_usuarios/lista.js.erb -> app/views/notificaciones_personas/lista.js.erb

--- app/models/notificacion_usuario.rb
+++ app/models/notificacion_usuario.rb
@@ -1,9 +1,9 @@
-class NotificacionUsuario < ActiveRecord::Base
+class NotificacionPersona < ActiveRecord::Base
   
-  self.table_name= "notificaciones_usuarios"
+  self.table_name= "notificaciones_personas"
   self.primary_key = "id"
   
-  attr_accessible :id, :descripcion, :usuario_id, :created_at, :updated_at
+  attr_accessible :id, :descripcion, :persona_id, :created_at, :updated_at
  
   scope :orden_01, -> { order("id")}
   scope :orden_descripcion, -> { order("descripcion")}
