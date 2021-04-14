87dac8605e84217b89fd18d2d98896938fd03d9d
Marcos Riveros <mriveros.py@gmail.com>
2021-04-13 22:38:46 -0400

    modified:   app/models/notificacion_persona.rb

--- app/models/notificacion_persona.rb
+++ app/models/notificacion_persona.rb
@@ -3,7 +3,7 @@ class NotificacionPersona < ActiveRecord::Base
   self.table_name= "notificaciones_personas"
   self.primary_key = "id"
   
-  attr_accessible :id, :descripcion, :persona_id, :created_at, :updated_at
+  attr_accessible :id, :descripcion, :persona_id, :created_at, :updated_at, :estado
  
   scope :orden_01, -> { order("id")}
   scope :orden_descripcion, -> { order("descripcion")}
