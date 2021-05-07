6ce1d3d83e79e8a15a64cf33ac84fd8755349778
Marcos Riveros <mriveros.py@gmail.com>
2021-05-07 09:30:20 -0400

    modified:   app/helpers/potreros_helper.rb 	modified:   app/views/potreros/_lista.html.erb

--- app/helpers/potreros_helper.rb
+++ app/helpers/potreros_helper.rb
@@ -11,5 +11,11 @@ module PotrerosHelper
     render partial: 'link_to_potrero_detalle', locals: { potrero_id: potrero_id}
 
   end
+
+  def link_to_potrero_geocerca(potrero_id)
+
+    render partial: 'link_to_potrero_geocerca', locals: { potrero_id: potrero_id}
+
+  end
   
 end
